493fbfbf61774d05cc75f2d3fe01cffdeab07ced
Manish Kumar <manishkumar80819@gmail.com>
2026-06-07 23:01:31 +0530

    Added Content in Views of App, connected it to project and displayed it

--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-
+from AppTwo import views
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls), 
+    path("", views.index),
 ]
